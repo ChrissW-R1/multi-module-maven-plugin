c50e6661e094c6e7c81701e2c0c7a19c6d30ccf3
ChrissW-R1 <contact@ChrissW-R1.me>
2025-07-20 16:07:45 +0200

    set session parameter to keep on obfuscation

--- src/main/java/me/chrisswr1/multimodulemavenplugin/ModuleRootPropertiesMojo.java
+++ src/main/java/me/chrisswr1/multimodulemavenplugin/ModuleRootPropertiesMojo.java
@@ -10,44 +10,46 @@ import org.apache.maven.project.MavenProject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import proguard.annotation.Keep;
+import proguard.annotation.KeepName;
 
 import java.nio.file.Path;
 
 @Mojo(
-    name = "module-root-properties",
-    defaultPhase = LifecyclePhase.INITIALIZE,
-    threadSafe = true
+	name = "module-root-properties",
+	defaultPhase = LifecyclePhase.INITIALIZE,
+	threadSafe = true
 )
 @Keep
 public class ModuleRootPropertiesMojo extends AbstractMojo {
-    @Parameter(
-        defaultValue = "${session}",
-        readonly = true
-    )
-    private MavenSession session;
+	@Parameter(
+		defaultValue = "${session}",
+		readonly = true
+	)
+	@KeepName
+	private MavenSession session;
 
-    @Override
-    public void execute() throws MojoExecutionException {
-        final @Nullable MavenProject topProject = session.getTopLevelProject();
-        if (topProject == null) {
-            throw new MojoExecutionException("Couldn't determine top level project!");
-        }
+	@Override
+	public void execute() throws MojoExecutionException {
+		final @Nullable MavenProject topProject = session.getTopLevelProject();
+		if (topProject == null) {
+			throw new MojoExecutionException("Couldn't determine top level project!");
+		}
 
-        final @Nullable String rootArtifactId = topProject.getArtifactId();
-        final @NotNull Path topDir = topProject.getBasedir().toPath();
-        final @NotNull Path currentDir = session.getCurrentProject().getBasedir().toPath();
-        @NotNull String relativePath = currentDir.relativize(topDir).toString();
-        if (relativePath.isEmpty()) {
-            relativePath = ".";
-        }
-        if (!(relativePath.endsWith("/"))) {
-            relativePath = relativePath + '/';
-        }
+		final @Nullable String rootArtifactId = topProject.getArtifactId();
+		final @NotNull Path topDir = topProject.getBasedir().toPath();
+		final @NotNull Path currentDir = session.getCurrentProject().getBasedir().toPath();
+		@NotNull String relativePath = currentDir.relativize(topDir).toString();
+		if (relativePath.isEmpty()) {
+			relativePath = ".";
+		}
+		if (!(relativePath.endsWith("/"))) {
+			relativePath = relativePath + '/';
+		}
 
-        getLog().info("Setting project.module-root.artifactId = " + rootArtifactId);
-        session.getUserProperties().setProperty("project.module-root.artifactId", rootArtifactId);
+		getLog().info("Setting project.module-root.artifactId = " + rootArtifactId);
+		session.getUserProperties().setProperty("project.module-root.artifactId", rootArtifactId);
 
-        getLog().info("Setting project.module-root.relativedir = " + relativePath);
-        session.getUserProperties().setProperty("project.module-root.relativedir", relativePath);
-    }
+		getLog().info("Setting project.module-root.relativedir = " + relativePath);
+		session.getUserProperties().setProperty("project.module-root.relativedir", relativePath);
+	}
 }
